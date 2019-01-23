edc5d2f18b406a29c555eae3f3f67e4d33a3d6e5
GiggleLiu <cacate0129@gmail.com>
2019-01-24 00:44:41 +0800

    fix su2 circuit for odd V

--- gradientcheck.jl
+++ gradientcheck.jl
@@ -11,12 +11,20 @@ function gradstat(hami; VER, Vmin, Vmax, R=10)
         nparam = chem.circuit |> nparameters
         println("Number of parameters = ", nparam)
         println("Number of vbits = ", V)
-        @time state_exact(chem)
-        @time state_exact(chem)
-        psi = state_exact(chem)
-        @show typeof(psi)
-        @time expect(hami, psi)
-        @time expect(hami, psi)
+        #@time state_exact(chem)
+        #@time state_exact(chem)
+        #psi = state_exact(chem)
+        #@show typeof(psi)
+        #@time expect(hami, psi)
+        #@time expect(hami, psi)
+
+        #@show expect(S2(16), state_exact(chem))
+        #@show chem.circuit
+        #@show collect(chem.circuit, Yao.Blocks.HGate) |> length
+        #@show collect(chem.circuit, Yao.Blocks.XGate) |> length
+        #@show collect(chem.circuit, Yao.Blocks.ControlBlock) |> length
+        #@show collect(chem.circuit, Yao.Blocks.Swap) |> length
+        #continue
 
         for k in 1:R
             dispatch!(chem.circuit, :random)
@@ -53,5 +61,19 @@ function QMPS.state_exact(chem::QuantumMPS{<:GPUReg})
         return psi0 |> circuit |> focus!((1:nbit-chem.nbit_ancilla)...) |> QMPS.remove_env!
     end
 end
+
+import Base:^
+^(A::AbstractBlock, n::Int) = chain(fill(A, n))
+
+"""total spin operator."""
+function S2(nbit::Int)
+    sum([sum([put(nbit, i=>G) for i=1:nbit])^2 for G in [X, Y, Z]])
+end
+
+@show expect(S2(2), zero_state(2) |> QMPS.singlet_block(ComplexF32, 2,1,2))
+@show expect(S2(2), register([0.0+0im,0,0,1]))
+@show expect(S2(2), register([1.0+0im,0,0,0]))
+@show expect(S2(2), register([0.0+0im,1,1,0]/sqrt(2)))
+
 const op = J1J2(4, 4; J2=0.5, periodic=false) |> hamiltonian
-gradstat(op; Vmin=1, Vmax=15, VER=:u1)
+gradstat(op; Vmin=1, Vmax=15, VER=:su2)
--- src/ansatz/su2_circuit.jl
+++ src/ansatz/su2_circuit.jl
@@ -8,19 +8,16 @@ function su2_circuit(::Type{T}, nbit_virtual::Int, nlayer::Int, nrepeat::Int, pa
     for i=1:nrepeat
         unit = chain(T, nbit_used)
         if i==1
-            for j=2+(nrepeat%2):2:nbit_virtual-1
-                @show j
+            for j=2-(nrepeat%2):2:nbit_virtual-1
                 push!(unit, singlet_block(T, nbit_used, j, j+1))
             end
         end
-        if i%2 == 1
-            if i==nrepeat
-                push!(unit, singlet_block(T, nbit_used, 1, 2))
-            else
-                push!(unit, singlet_block(T, nbit_used, 1, nbit_used))
-            end
+        if i%2 != nrepeat%2
+            push!(unit, singlet_block(T, nbit_used, 1, nbit_used))
         else
-            push!(unit, swap(nbit_used, T, 1, nbit_used))   # fix swap parameter order!
+            if i!=1
+                push!(unit, swap(nbit_used, T, 1, nbit_used))   # fix swap parameter order!
+            end
         end
         for j=1:nlayer
             nring = nbit_virtual
